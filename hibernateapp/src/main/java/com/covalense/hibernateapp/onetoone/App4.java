fa3136d43b52681d9ae3098627a69d609984119d
rameshcovalanse <indrapratapyadav4398@gmail.com>
2019-08-06 19:33:10 +0530

    Hibernate Mapping

--- hibernateapp/src/main/java/com/covalense/hibernateapp/onetoone/App4.java
+++ hibernateapp/src/main/java/com/covalense/hibernateapp/onetoone/App4.java
@@ -5,6 +5,10 @@ import java.util.Date;
 
 import com.covalense.hibernateapp.beans.EmployeeAddressInfoBean;
 import com.covalense.hibernateapp.beans.EmployeeAdreessPKBean;
+import com.covalense.hibernateapp.beans.EmployeeEducationalInfoBean;
+import com.covalense.hibernateapp.beans.EmployeeEducationalInfoPKBean;
+import com.covalense.hibernateapp.beans.EmployeeExperienceInfoBean;
+import com.covalense.hibernateapp.beans.EmployeeExperienceInfoPKBean;
 import com.covalense.hibernateapp.beans.EmployeeInfoBean;
 import com.covalense.hibernateapp.beans.EmployeeOtherInfoBean;
 
@@ -61,6 +65,53 @@ public class App4 {
 		addressInfoBean2.setAddress2("BTM-2");
 		addressInfoBean2.setCity("Bangalore");
 		addressInfoBean2.setLandmark("Manjunaa");
+		
+		EmployeeEducationalInfoPKBean educationalInfoPKBean1 = new EmployeeEducationalInfoPKBean();
+		educationalInfoPKBean1.setEducationalType("Regular");
+		educationalInfoPKBean1.setInfoBean(empInf);
+
+		EmployeeEducationalInfoPKBean educationalInfoPKBean2 = new EmployeeEducationalInfoPKBean();
+		educationalInfoPKBean2.setEducationalType("Distance");
+		educationalInfoPKBean2.setInfoBean(empInf);
+
+		EmployeeEducationalInfoBean educationalInfoBean1 = new EmployeeEducationalInfoBean();
+		educationalInfoBean1.setEducationalInfoPKBean(educationalInfoPKBean1);
+		educationalInfoBean1.setCollegeName("Bhabha");
+		educationalInfoBean1.setUniversity("CCNUL");
+		educationalInfoBean1.setLoaction("India");
+		educationalInfoBean1.setPercentage(76.9);
+		educationalInfoBean1.setYop(new Date(2010 - 06 - 02));
+
+		EmployeeEducationalInfoBean educationalInfoBean2 = new EmployeeEducationalInfoBean();
+		educationalInfoBean2.setEducationalInfoPKBean(educationalInfoPKBean2);
+		educationalInfoBean2.setCollegeName("Sakara");
+		educationalInfoBean2.setUniversity("CUR");
+		educationalInfoBean2.setLoaction("India");
+		educationalInfoBean2.setPercentage(76.9);
+		educationalInfoBean2.setYop(new Date(2014 - 06 - 12));
+
+		EmployeeExperienceInfoPKBean experienceInfoPKBean1 = new EmployeeExperienceInfoPKBean();
+		experienceInfoPKBean1.setCompanyName("ABCDF");
+		experienceInfoPKBean1.setInfoBean(empInf);
+
+		EmployeeExperienceInfoPKBean experienceInfoPKBean2 = new EmployeeExperienceInfoPKBean();
+		experienceInfoPKBean2.setCompanyName("PQRST");
+		experienceInfoPKBean2.setInfoBean(empInf);
+
+		EmployeeExperienceInfoBean experienceInfoBean1 = new EmployeeExperienceInfoBean();
+		experienceInfoBean1.setExperienceInfoPKBean(experienceInfoPKBean1);
+		experienceInfoBean1.setDesignation("SoftwareEngineer");
+		experienceInfoBean1.setJoiningDate(new Date(2016 - 06 - 12));
+		experienceInfoBean1.setJoiningDate(new Date(2017 - 03 - 12));
+
+		EmployeeExperienceInfoBean experienceInfoBean2 = new EmployeeExperienceInfoBean();
+		experienceInfoBean2.setExperienceInfoPKBean(experienceInfoPKBean2);
+		experienceInfoBean2.setDesignation("Testing Engineer");
+		experienceInfoBean2.setJoiningDate(new Date(2015 - 06 - 12));
+		experienceInfoBean2.setJoiningDate(new Date(2019 - 03 - 12));
+		
+		//empInf.setEmployeeExperienceInfoBean(Arrays.asList(experienceInfoBean1,experienceInfoBean2));
+		empInf.setEmployeeEducationalInfoBeans(Arrays.asList(educationalInfoBean1,educationalInfoBean2));
 		
 		empInf.setAddressInfoBeans(Arrays.asList(addressInfoBean1,addressInfoBean2));
 
